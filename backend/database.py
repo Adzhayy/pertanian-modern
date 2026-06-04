336022819e77e909606f0696d3b103dfb71ff304
taufiq <aahmadtaufiqul@gmail.com>
2026-06-04 16:05:47 +0700

    fix: memaksa baca URL awan

--- backend/database.py
+++ backend/database.py
@@ -4,13 +4,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Memuat variabel rahasia dari file .env
-load_dotenv()
+# 1. Coba ambil kunci URL dari server awan (Railway) TERLEBIH DAHULU
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL", 
-    "mysql+pymysql://root:@127.0.0.1:3306/pertanian-modern" # <-- Ganti dengan nama db lokalmu
-)
+# 2. Jika di awan kosong (berarti sedang dijalankan di laptop), baru pakai lokal
+if not DATABASE_URL:
+    load_dotenv() # Muat file .env lokal
+    DATABASE_URL = os.getenv(
+        "DATABASE_URL", 
+        "mysql+pymysql://root:@127.0.0.1:3306/pertanian-modern" # Default lokal
+    )
 
 # Membuat 'mesin' penghubung ke MySQL
 engine = create_engine(DATABASE_URL)
