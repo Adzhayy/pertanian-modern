9dbada25bf52d6dad6060d540ec730453070bad3
taufiq <aahmadtaufiqul@gmail.com>
2026-06-09 13:48:42 +0700

    menambahkan import get_current_user yang tertinggal

--- backend/routes/auth_user.py
+++ backend/routes/auth_user.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from fastapi.security import OAuth2PasswordRequestForm
 import models, schemas, auth
 from database import get_db
+from auth import get_current_user
 
 router = APIRouter()
 
